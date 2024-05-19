d04d91235cf7e18f998da2b0a3992bd5dfc4c514
DArt <madim-neodart@rambler.ru>
2024-05-19 12:55:49 +0300

    Main file

---
+++ opencv4.cpp
@@ -0,0 +1,15 @@
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+
+
+int main()
+{
+ std::string path
+ cv::imread   
+
+    return 0;
+}
