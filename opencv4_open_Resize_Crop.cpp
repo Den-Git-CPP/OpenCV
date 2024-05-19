41d3911b70ec5c24667fb54ba31d3a91bb48e6d1
DArt <madim-neodart@rambler.ru>
2024-05-19 16:17:20 +0300

    Work with size

--- opencv4_open_Resize_Crop.cpp
+++ opencv4_open_Resize_Crop.cpp
@@ -9,11 +9,18 @@ int main ()
     // Open Image
     std::string path = "../resources_opencv/img1.png";
     cv::Mat img      = cv::imread (path);
-  
-  
+    cv::Mat imgResize, imgResizeHalf;
+    cv::resize (img, imgResize, cv::Size (640, 480));
+    cv::resize (img, imgResizeHalf, cv::Size (), 0.5, 0.5);
     // show
+
+    std::cout << "Image " << img.size () << "\n";
+    std::cout << "imgResize " << imgResize.size () << "\n";
+    std::cout << "imgResizeHalf " << imgResizeHalf.size () << "\n";
+
     cv::imshow ("Image", img);
-    
+    cv::imshow ("ImgResize", imgResize);
+    cv::imshow ("imgResizeHalf", imgResizeHalf);
 
     if (cv::waitKey (0) == 27) {
         std::cout << "Esc key is pressed by user. Stoppig the image\n";
