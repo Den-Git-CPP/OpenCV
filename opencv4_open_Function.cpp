0ee9ee8488a1bff9963b4202554bff40d01b4627
DArt <madim-neodart@rambler.ru>
2024-05-19 15:20:05 +0300

    Work with Function cvtColor GaussianBlur

--- opencv4_open_Function.cpp
+++ opencv4_open_Function.cpp
@@ -9,10 +9,15 @@ int main ()
     // Open Image
     std::string path = "../resources_opencv/img1.png";
     cv::Mat img      = cv::imread (path);
-    cv::Mat imgGray;
-    cv::cvtColor(img,imgGray,cv::COLOR_BGRA2GRAY);// серый
-    cv::imshow ("Image", imgGray);
+    cv::Mat imgGray, imgBlur;
+    cv::cvtColor (img, imgGray, cv::COLOR_BGRA2GRAY);         // серый
+    cv::GaussianBlur (img, imgBlur, cv::Size (5, 5), 5, 0); //
     
+    // show
+    cv::imshow ("Image", img);
+    cv::imshow ("ImageGray", imgGray);
+    cv::imshow ("imgBlur", imgBlur);
+
     if (cv::waitKey (0) == 27) {
         std::cout << "Esc key is pressed by user. Stoppig the image\n";
         exit;
