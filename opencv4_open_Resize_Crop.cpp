944f01c8a6f52366d3d7a620e968c42f9e6c8c33
DArt <madim-neodart@rambler.ru>
2024-05-19 16:25:14 +0300

    work with Crop

--- opencv4_open_Resize_Crop.cpp
+++ opencv4_open_Resize_Crop.cpp
@@ -10,17 +10,24 @@ int main ()
     std::string path = "../resources_opencv/img1.png";
     cv::Mat img      = cv::imread (path);
     cv::Mat imgResize, imgResizeHalf;
+    cv::Mat imgCrop;
+    // Resize
     cv::resize (img, imgResize, cv::Size (640, 480));
     cv::resize (img, imgResizeHalf, cv::Size (), 0.5, 0.5);
-    // show
+    // Crop
+    cv::Rect roi (100, 100, 300, 250);
+    imgCrop = img (roi);
 
+    // show
     std::cout << "Image " << img.size () << "\n";
     std::cout << "imgResize " << imgResize.size () << "\n";
     std::cout << "imgResizeHalf " << imgResizeHalf.size () << "\n";
+    std::cout << "imgCrop " << imgCrop.size () << "\n";
 
     cv::imshow ("Image", img);
     cv::imshow ("ImgResize", imgResize);
     cv::imshow ("imgResizeHalf", imgResizeHalf);
+    cv::imshow ("imgCrop", imgCrop);
 
     if (cv::waitKey (0) == 27) {
         std::cout << "Esc key is pressed by user. Stoppig the image\n";
