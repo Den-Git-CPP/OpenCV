4574be70987584fe46d62ea63b0a7fdc2a8ddc39
DArt <madim-neodart@rambler.ru>
2024-05-19 17:16:51 +0300

    Work Text

--- opencv4_open_Drawing_Shape_Text.cpp
+++ opencv4_open_Drawing_Shape_Text.cpp
@@ -7,10 +7,23 @@
 int main ()
 {
     // Open blank Image
-     cv::Mat img(512,512,cv::CV_8UC3,cv::Scalar(255,0,0));
+    cv::Mat img (512, 512, CV_8UC3, cv::Scalar (255, 255, 255));
+    // draw circle
+    cv::circle (img, cv::Point (50, 50), 25, cv::Scalar (139,0,0));
+    cv::circle (img, cv::Point (150, 50), 25, cv::Scalar (139,0,0), 10);
+    cv::circle (img, cv::Point (250, 50), 25, cv::Scalar (139,0,0), cv::FILLED);
+    // draw rectangle
+    cv::rectangle (img, cv::Point (25, 150), cv::Point (125, 100), cv::Scalar (139,0,0));
+    cv::rectangle (img, cv::Point (150, 150), cv::Point (250, 100), cv::Scalar (139,0,0),3);
+    cv::rectangle (img, cv::Point (275, 150), cv::Point (375, 100), cv::Scalar (139,0,0),cv::FILLED);
+     
+         // draw line
+ cv::line (img, cv::Point (25, 200), cv::Point (400, 200), cv::Scalar (139,0,0));
+ // draw text
+ cv::putText(img,"Work with text-1",cv::Point (25, 250),cv::FONT_HERSHEY_DUPLEX,0.75,cv::Scalar (139,0,0));
+cv::putText(img,"Work with text-2",cv::Point (25, 300),cv::FONT_HERSHEY_DUPLEX,0.75,cv::Scalar (139,0,0),2);
 
-
-    // Show
+       // Show
     cv::imshow ("Image", img);
     // Close
     if (cv::waitKey (0) == 27) {
