05b4212d67f4bd711d46b322d8ffe689fd64471a
DArt <madim-neodart@rambler.ru>
2024-05-21 00:30:23 +0300

    Work with detect  orange color

--- opencv4_open_Color_Detection.cpp
+++ opencv4_open_Color_Detection.cpp
@@ -7,13 +7,22 @@
 int main ()
 {
     // Open Image
-    std::string path = "../resources_opencv/img2.png";
-    cv::Mat img      = cv::imread (path);
+    std::string path = "../resources_opencv/DetectColor.png";
+    cv::Mat imgHSV, mask;
+    int hmin{ 0 }, smin{ 110 }, vmin{ 153 };
+    int hmax{ 19 }, smax{ 240 }, vmax{ 255 };
+    cv::Mat img = cv::imread (path);
     // обход  трансформируемой фигуры по часовой коодинат по часовой
-     
-     
+    cv::cvtColor (img, imgHSV, cv::COLOR_BGR2HSV); // серый
+    cv::Scalar lower (hmin, smin, vmin);
+    cv::Scalar upper (hmax, smax, vmax);
+    cv::inRange (imgHSV, lower, upper, mask);
+
     cv::imshow ("Image", img);
-      //  Close
+    cv::imshow ("Mask", mask);
+    cv::imshow ("imgHSV", imgHSV);
+
+    //  Close
     if (cv::waitKey (0) == 27) {
         std::cout << "Esc key is pressed by user. Stoppig the show image\n";
         exit;
