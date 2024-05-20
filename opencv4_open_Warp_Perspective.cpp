27551840d87bcdbb34a55cc15b2784659c7b0960
DArt <madim-neodart@rambler.ru>
2024-05-20 23:26:56 +0300

    Work warpPerspective

--- opencv4_open_Warp_Perspective.cpp
+++ opencv4_open_Warp_Perspective.cpp
@@ -6,11 +6,32 @@
 
 int main ()
 {
-     // Open Image
+    float w = 250;
+    float h = 350;
+    cv::Mat matrix, imgWrap;
+    // Open Image
     std::string path = "../resources_opencv/img1.png";
     cv::Mat img      = cv::imread (path);
-    cv::imshow ("Image", img);
-    
+    // обход  трансформируемой фигуры по часовой коодинат по часовой
+    cv::Point2f src [4] = {
+        {444,  65},
+        {527,  65},
+        {527, 143},
+        {444, 143}
+    };
+
+    cv::Point2f dst [4] = {
+        {0.0f, 0.0f},
+        {   w, 0.0f},
+        {0.0f,    h},
+        {   w,    h}
+    };
+
+    matrix = getPerspectiveTransform (src, dst);
+    cv::warpPerspective (img, imgWrap, matrix, cv::Point (w, h));
+
+   // cv::imshow ("Image", img);
+    cv::imshow ("Image", imgWrap);
     // Close
     if (cv::waitKey (0) == 27) {
         std::cout << "Esc key is pressed by user. Stoppig the show image\n";
