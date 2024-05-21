096bf55c8d62d3a2b6a7b8d455eda1fc7faf5700
DArt <madim-neodart@rambler.ru>
2024-05-21 12:43:46 +0300

    trackbar

--- opencv4_open_Color_Detection.cpp
+++ opencv4_open_Color_Detection.cpp
@@ -3,29 +3,40 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
-
+    int hmin{ 0 }, smin{ 0 }, vmin{ 0 };
+    int hmax{ 255 }, smax{ 255 }, vmax{ 255 };
 int main ()
 {
     // Open Image
     std::string path = "../resources_opencv/DetectColor.png";
     cv::Mat imgHSV, mask;
-    int hmin{ 0 }, smin{ 110 }, vmin{ 153 };
-    int hmax{ 19 }, smax{ 240 }, vmax{ 255 };
+
+
     cv::Mat img = cv::imread (path);
-    // обход  трансформируемой фигуры по часовой коодинат по часовой
     cv::cvtColor (img, imgHSV, cv::COLOR_BGR2HSV); // серый
-    cv::Scalar lower (hmin, smin, vmin);
-    cv::Scalar upper (hmax, smax, vmax);
-    cv::inRange (imgHSV, lower, upper, mask);
+    //Трекбары для поиска значений фильтров
+    cv::namedWindow ("Trackbars", (640, 200));
+    cv::createTrackbar ("Hue Min", "Trackbars", &hmin, 179);
+    cv::createTrackbar ("Hue Max", "Trackbars", &hmax, 179);
+    cv::createTrackbar ("Sat Min", "Trackbars", &smin, 255);
+    cv::createTrackbar ("Sat Max", "Trackbars", &smax, 255);
+    cv::createTrackbar ("Val Min", "Trackbars", &vmin, 255);
+    cv::createTrackbar ("Val Max", "Trackbars", &vmax, 255);
 
-    cv::imshow ("Image", img);
-    cv::imshow ("Mask", mask);
-    cv::imshow ("imgHSV", imgHSV);
+    while (true) {
+        cv::Scalar lower (hmin, smin, vmin);
+        cv::Scalar upper (hmax, smax, vmax);
+        cv::inRange (imgHSV, lower, upper, mask);
 
-    //  Close
-    if (cv::waitKey (0) == 27) {
-        std::cout << "Esc key is pressed by user. Stoppig the show image\n";
-        exit;
+        cv::imshow ("Image", img);
+        cv::imshow ("Mask", mask);
+        cv::imshow ("imgHSV", imgHSV);
+
+        //  Close
+        if (cv::waitKey (0) == 27) {
+            std::cout << "Esc key is pressed by user. Stoppig the show image\n";
+            break;
+        }
     }
 
     return 0;
